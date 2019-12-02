040ec898b9222e62e54f9c89b31799ab0bee51fd
Alastair Smith <github@alastairsmith.me.uk>
2019-12-02 19:57:14 +0000

    Initial project setup

---
+++ 2019/2/Intcode/Intcode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Intcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
