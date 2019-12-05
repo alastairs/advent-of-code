a0ac07f4b90237785490e8c899e76aaafbd99d81
Alastair Smith <github@alastairsmith.me.uk>
2019-12-05 21:19:28 +0000

    dotnet new

---
+++ 2019/3/CrossedWires/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CrossedWires
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
