9a8734c6b8295fe1359ac68c17a6f222a79aab6d
Alastair Smith <github@alastairsmith.me.uk>
2019-12-07 21:50:53 +0000

    dotnet new

---
+++ 2019/6/OrbitMap/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OrbitMap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
